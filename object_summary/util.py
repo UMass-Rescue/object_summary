1815e7363719fd51a3aedecc7ebf4dc8b7edde23
prasannals <prasanna.pessu@gmail.com>
2020-02-23 19:42:16 -0500

    Added tree visualization function + objects_in_categories_df

--- object_summary/util.py
+++ object_summary/util.py
@@ -1,6 +1,11 @@
 import pandas as pd
 import numpy as np
 
+from sklearn.tree import export_graphviz
+import pydotplus
+from six import StringIO 
+from IPython.display import Image
+
 def split_df(df, num_splits:int):
     '''
     df - pandas DataFrame object
@@ -20,3 +25,21 @@ def split_df(df, num_splits:int):
         parts.append(df.iloc[start:end])
         
     return parts
+
+def tree_viz(model: 'Decision Tree model', 
+             class_names: 'list(str) - list of label names', 
+             feature_names: 'list(str)', 
+             out_fname:'if specified, graph is saved to this path'=None) -> Image:
+    dot_data = StringIO()
+    export_graphviz(model, 
+     out_file=dot_data, 
+     class_names=class_names, 
+     feature_names=feature_names,
+     filled=True,
+     rounded=True,
+     special_characters=True)
+
+    graph = pydotplus.graph_from_dot_data(dot_data.getvalue()) 
+    if out_fname:
+        graph.write_png(out_fname)
+    return Image(graph.create_png())
